e6d5fa3217c8f7121ac1e0b554e50c5413d81587
Seike Takumi <takoforest+github@gmail.com>
2013-05-05 03:19:26 +0900

    emacs.d

--- .emacs
+++ .emacs
@@ -1,10 +1,3 @@
-;=======================================
-; espresso-mode for jQuery
-;=======================================
-(add-to-list 'load-path "~/.emacs.d")
-(autoload #'espresso-mode "espresso" "Start espresso-mode" t)
-(add-to-list 'auto-mode-alist '("\\.js$" . espresso-mode))
-(add-to-list 'auto-mode-alist '("\\.json$" . espresso-mode))
 ;行番号の表示
 (global-linum-mode t)
 ;prevent Backup
@@ -19,6 +12,6 @@
 (require 'ido)
 (ido-mode t)
 
- (setq-default tab-width 4)
- (setq default-tab-width 4)
-
+(setq-default indent-tabs-mode nil)
+(setq-default js2-basic-offset 2)
+(add-hook 'js2-mode-hook 'js-indent-hook)
---
+++ .emacs.d/init.el
@@ -0,0 +1,48 @@
+;; LoadPathまわり
+(add-to-list 'load-path "~/.emacs.d/elisp")
+
+(when (> emacs-major-version 23)
+  (defvar user-emacs-directory "~/.emacs.d"))
+
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+      (let ((default-directory
+              (expand-file-name (concat user-emacs-directory path))))
+        (add-to-list 'load-path default-directory)
+        (if (fboundp ' normal-top-level-add-subdirs-to-load-path)
+            (normal-top-level-add-subdirs-to-load-path))))))
+(add-to-load-path "elisp" "conf" "public_repos")
+
+;;auto-installの設定
+(when(require 'auto-install nil t)
+  ;;インストールディレクトリを設定する　初期値は~/.emacs.d/auto-install/
+  (setq auto-install-directory "~/.emacs.d/elisp/")
+  ;;EmacsWikiに登録されているelispの名前を取得する
+  (auto-install-update-emacswiki-package-name t)
+  ;;必要であればプロキシの設定を行う
+  ;;(setq url-proxy-services '(("http" . "localhost:8339")))
+  ;;install-elispの関数を利用可能にする
+  (auto-install-compatibility-setup))
+
+;; package.el(for emacs 23)
+(when ( require 'package nil t)
+  (add-to-list 'package-archives
+               '("marmalade" . "http://marmalade-repo.org/packages/"))
+  (package-initialize))
+;;;; UserConfig
+;; indent
+(setq-default indent-tabs-mode nil)
+(setq c-basic-offset 2)
+;; no-backup
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+;;js2-mode
+(autoload 'js2-mode "js2-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
+;;auto-complete
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/elpa/auto-complete-1.4/dict/")
+
+(ac-config-default)
